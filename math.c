0a695dc6b8fa19bdebf30a6381aa4056f51209f7
OneHunderGame <100gamesdev@gmail.com>
2026-02-22 02:57:38 +0200

    The calculator file
    this is the code for calculator it inside a terminal

---
+++ math.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    float number = 2;
+
+    //number = sqrt(number);
+    //number = pow(number, 3);
+    //number = round(number);
+    //number = ceil(number);
+    //number = floor(number);
+    //number = abs(number);
+    //number = log(number);
+    //mber = sin(number);
+    //number = cos(number);
+    //number = tan(number);
+
+    printf("%.2f\n", number);
+
+    return 0;
+}
